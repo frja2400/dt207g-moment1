7a2fc562dae3d3a6b65cd44c281af57a8e967e80
Frida Jansson <frja2400@student.miun.se>
2025-04-12 18:57:05 +0200

    first commit

---
+++ install.js
@@ -0,0 +1,22 @@
+//Importerar SQLite-databasen och aktiverar detaljerad inloggning med verbose.
+const sqlite3 = require("sqlite3").verbose();
+
+//Skapa databas
+const db = new sqlite3.Database("./db/courses.db");
+
+//Skapa tabell. Seralize säkerställer att kommandona exekveras i den ordning de skrivs.
+db.serialize(() => {
+    db.run("DROP TABLE IF EXISTS courses;");
+
+    db.run(`
+        CREATE TABLE courses(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            coursecode VARCHAR(50) UNIQUE,
+            coursename VARCHAR(80),
+            syllabus VARCHAR(255),
+            progression CHAR(1)
+        );
+    `);
+});
+
+db.close();
