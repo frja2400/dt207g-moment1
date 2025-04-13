54a8eaf59c9f79f189d31d2291eeecd96c02cf1e
Frida Jansson <frja2400@student.miun.se>
2025-04-13 18:01:52 +0200

    Gjorde lite ändringar i install-fil och script-tag inför publicering.

--- install.js
+++ install.js
@@ -6,16 +6,15 @@ const db = new sqlite3.Database("./db/courses.db");
 
 //Skapa tabell. Seralize säkerställer att kommandona exekveras i den ordning de skrivs.
 db.serialize(() => {
-    db.run("DROP TABLE IF EXISTS courses;");
 
     db.run(`
-        CREATE TABLE courses(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            coursecode VARCHAR(50) UNIQUE,
-            coursename VARCHAR(80),
-            syllabus VARCHAR(255),
-            progression CHAR(1)
-        );
+        CREATE TABLE IF NOT EXISTS courses (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        coursecode VARCHAR(50) UNIQUE,
+        coursename VARCHAR(80),
+        syllabus VARCHAR(255),
+        progression CHAR(1)
+  );
     `);
 });
 
